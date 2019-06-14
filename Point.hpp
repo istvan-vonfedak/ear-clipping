f31e5c7221b413be330b538d39a9d7d0d00b0cba
Istvan von Fedak <istvan.vonfedak@gmail.com>
2019-06-14 13:22:40 -0400

    Update Point.hpp

--- Point.hpp
+++ Point.hpp
@@ -5,14 +5,20 @@ struct Point {
   double x, y;
 
   Point(): x(0), y(0) {}
+  
   Point(const Point & p): x(p.x), y(p.y) {}
+  
   Point(double px, double py): x(px), y(py) {}
+  
   Point operator + (const Point & p) const
   { return Point(x + p.x, y + p.y); }
+  
   Point operator - (const Point & p) const
   { return Point(x - p.x, y - p.y); }
+  
   Point operator * (const Point & p) const
   { return Point(x * p.x, y * p.y); }
+  
   Point operator / (const Point & p) const
   { return Point(x / p.x, y / p.y); }
 
